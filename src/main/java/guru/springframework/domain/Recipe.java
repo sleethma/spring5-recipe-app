3af197e84e00aed04b3a821200a976c68e0a1211
Michael Sleeth <sleeth.ma@gmail.com>
2018-12-28 02:53:43 -0600

    Adds generated Id values for DB

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,19 @@
 package guru.springframework.domain;
 
-public class Recipes {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Recipe {
+
+    //@GenType: supports autogen for id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String description;
     private Integer prepTime;
     private Integer cookTime;
@@ -10,6 +23,9 @@ public class Recipes {
     private String directions;
     //todo: add difficulty
 
+    @Lob //enables large object field
     private Byte[] images;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 }
