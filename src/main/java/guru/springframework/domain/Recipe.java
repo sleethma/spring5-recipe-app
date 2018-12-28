ed77dbd4b6378a12b98ea7bae4617fb4c6a14f4a
Michael Sleeth <sleeth.ma@gmail.com>
2018-12-27 22:19:47 -0600

    Adds Entities to Notes and Recipies, establishes relationship with JPA

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,15 @@
+package guru.springframework.domain;
+
+public class Recipes {
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo: add difficulty
+
+    private Byte[] images;
+    private Notes notes;
+}
